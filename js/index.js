d91ad63f5239d3d02a97bf4a15ebc2e836b38b54
Pisarski Roman <158080851+RomanPisarski@users.noreply.github.com>
2024-01-30 19:42:15 +0100

    galeria

--- js/index.js
+++ js/index.js
@@ -1,9 +1,24 @@
 jQuery(document).ready(function () {
-  jQuery("#my_nanogallery2").nanogallery2({
+  jQuery(".gallery").nanogallery2({
     items: [
       { src: "/20240124_203538.jpg", srct: "/20240124_203538.jpg", title: "Karolina i Robert" },
       { src: "/20240124_203540.jpg", srct: "/20240124_203540.jpg", title: "Karolina i Robert" },
-      { src: "/20240124_203543.jpg", srct: "/20240124_203543.jpg", title: "Karolina i Robert" }
+      { src: "/20240124_203543.jpg", srct: "/20240124_203543.jpg", title: "Karolina i Robert" },
+
+      { src: "/20240124_203546.jpg", srct: "/20240124_203546.jpg", title: "Karolina i Robert" },
+      { src: "/20240124_203601.jpg", srct: "/20240124_203601.jpg", title: "Karolina i Robert" },
+      { src: "/20240125_004534.jpg", srct: "/20240125_004534.jpg", title: "Grażynka i Roman" },
+
+      { src: "/20240125_004539.jpg", srct: "/20240125_004539.jpg", title: "Grażynka, Robert i Roman" },
+      { src: "/20240125_004550.jpg", srct: "/20240125_004550.jpg", title: "Robert i Roman" },
+      { src: "/20240125_004552.jpg", srct: "/20240125_004552.jpg", title: "Robert i Roman" },
+
+      { src: "/20240125_004607.jpg", srct: "/20240125_004607.jpg", title: "Grażynka, Kacper i Roman" },
+      { src: "/20240125_004642.jpg", srct: "/20240125_004642.jpg", title: "Kasia i Roman" },
+      { src: "/20240125_004714.jpg", srct: "/20240125_004714.jpg", title: "Pisarscy" },
+
+      { src: "/20240125_020550.jpg", srct: "/20240125_020550.jpg", title: "Kasia i Kacper" },
+      { src: "/20240125_023645.jpg", srct: "/20240125_023645.jpg", title: "Marzena, Kasia i Roman" },
     ],
     thumbnailWidth: "auto",
     thumbnailHeight: 100,
