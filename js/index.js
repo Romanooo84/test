4754a8d42af6285d6ccdf2ad66a5a5630dd0d36b
Pisarski Roman <158080851+RomanPisarski@users.noreply.github.com>
2024-01-30 19:46:53 +0100

    commit

--- js/index.js
+++ js/index.js
@@ -21,9 +21,41 @@ jQuery(document).ready(function () {
       { src: "/20240125_023645.jpg", srct: "/20240125_023645.jpg", title: "Marzena, Kasia i Roman" },
     ],
     thumbnailWidth: "auto",
-    thumbnailHeight: 100,
+    thumbnailHeight: 300,
     itemsBaseURL: "/photos",
+    galleryDisplayMode: 'pagination',
+    galleryMaxRows: 3, 
+    gallerySorting: 'random',
+    thumbnailAlignment: 'fillWidth',
+          thumbnailL1GutterWidth: 20,
+          thumbnailL1GutterHeight: 20,
+          thumbnailBorderHorizontal: 1,
+          thumbnailBorderVertical: 1,
 
-    locationHash: false
+          // THUMBNAIL TOOLS & LABEL
+          thumbnailL1Label: { display: true, position:'overImageOnTop', hideIcons: true, titleFontSize: '1.5em', align: 'left'},
+          thumbnailToolbarImage :  { topLeft: 'select', bottomRight : 'featured,display,download,info,cart' },
+
+          // DISPLAY ANIMATION
+          thumbnailDisplayTransition: 'flipUp',       // thumbnail display animation
+          thumbnailDisplayTransitionDuration: 400,
+          thumbnailDisplayInterval: 200,
+          thumbnailDisplayOrder: 'rowByRow',
+
+          // THUMBNAIL'S HOVER ANIMATION
+          thumbnailHoverEffect2: 'toolsSlideUp|labelSlideDown',
+          touchAnimation: true,
+          touchAutoOpenDelay: -1,
+
+          // GALLERY THEME
+          galleryTheme : { 
+            thumbnail: { titleShadow : 'none', descriptionShadow : 'none', titleColor: '#fff', borderColor: '#fff' },
+            navigationPagination :  { background: '#3C4B5B', color: '#fff', colorHover: '#aaa', borderRadius: '4px' },
+          },
+        
+          
+          // DEEP LINKING
+          locationHash: false
+
   });
 });
